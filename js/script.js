3962d8a48e0ad7fc40b61873663da09d970a9b33
IVENTEYE <52000764+IVENTEYE@users.noreply.github.com>
2021-01-08 02:59:19 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -695,6 +695,274 @@ if (document.querySelectorAll('.inside-slider__work').length > 0) {
 	  });
 }
 
+if (document.querySelectorAll('.author__card-content').length > 0) {
+	$('.author__card-content').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev'),
+		nextArrow: $('.author__card-arrows-item--next'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--2').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-2'),
+		nextArrow: $('.author__card-arrows-item--next-2'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--3').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-3'),
+		nextArrow: $('.author__card-arrows-item--next-3'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--4').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-4'),
+		nextArrow: $('.author__card-arrows-item--next-4'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--5').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-5'),
+		nextArrow: $('.author__card-arrows-item--next-5'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--6').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-6'),
+		nextArrow: $('.author__card-arrows-item--next-6'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--7').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-7'),
+		nextArrow: $('.author__card-arrows-item--next-7'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--8').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-8'),
+		nextArrow: $('.author__card-arrows-item--next-8'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--9').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-9'),
+		nextArrow: $('.author__card-arrows-item--next-9'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--10').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-10'),
+		nextArrow: $('.author__card-arrows-item--next-10'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--11').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-11'),
+		nextArrow: $('.author__card-arrows-item--next-11'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--12').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-12'),
+		nextArrow: $('.author__card-arrows-item--next-12'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--13').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-13'),
+		nextArrow: $('.author__card-arrows-item--next-13'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+
+	  $('.author__card-content--14').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		infinite: false,
+		prevArrow: $('.author__card-arrows-item--prev-14'),
+		nextArrow: $('.author__card-arrows-item--next-14'),
+		responsive: [
+			{
+				breakpoint: 426,
+				settings: {
+					centerMode: false,
+					arrows: false,
+				}
+			},
+		]
+	  });
+}
+
 //================================================Adaptive js=============================================================
 
 (function () {
@@ -966,31 +1234,6 @@ if (document.querySelector('.tabs')) {
 
 // =========================================================Spoils=============================================================
 
-// if (document.querySelectorAll('.staps-spoil').length > 0) {
-// 	const spoils = document.querySelectorAll('.staps-spoil');
-// 	for (spoil of spoils) {
-// 		const spoilHeader = spoil.querySelector('.staps-spoil__header'),
-// 		spoilbody = spoil.querySelector('.staps-spoil__body'),
-// 		spoilClose = spoil.querySelector('.staps-spoil__header-btn');
-// 		if (!spoilbody.hasChildNodes()) {
-// 			spoilClose.style.display = 'none';
-// 			spoilbody.style.display = 'none';
-// 			spoilHeader.classList.remove('active');
-// 		} else {
-// 			spoilClose.style.display = 'block';
-// 		}
-// 		spoil.addEventListener('click', () => {
-// 			spoilHeader.classList.toggle('active');
-// 			if (!spoilbody.hasChildNodes()) {
-// 				spoilbody.style.display = 'none';
-// 				spoilHeader.classList.remove('active');
-// 			} else {
-// 				$(spoilbody).slideToggle();
-// 			}
-// 		});
-// 	}
-// }
-
 if (document.querySelectorAll('.tabs').length > 0) {
 	const tabsProduct = document.querySelectorAll('.tabcontent-application__item'),
 		  tabsNav = document.querySelectorAll('.tabnav-application__item');
@@ -1200,18 +1443,20 @@ if (document.querySelector('.alerts__header-btn')) {
 
 const publicationActions = document.querySelectorAll('.publication-actions');
 
-const checkInput = () => {
-	for (publicationAction of publicationActions) {
-			const publicationCheckbox = publicationAction.querySelector('input');
-			if (publicationCheckbox.checked) {
-				publicationAction.classList.add('active');
-			} else {
-				publicationAction.classList.remove('active');
-			}
-	}
-};
-
-publicationActions.forEach(item => {
-	item.addEventListener('click', checkInput);
-});
+if (publicationActions.length > 0) {
+	const checkInput = () => {
+		for (publicationAction of publicationActions) {
+				const publicationCheckbox = publicationAction.querySelector('input');
+				if (publicationCheckbox.checked) {
+					publicationAction.classList.add('active');
+				} else {
+					publicationAction.classList.remove('active');
+				}
+		}
+	};
+	
+	publicationActions.forEach(item => {
+		item.addEventListener('click', checkInput);
+	});
+}
 	  
